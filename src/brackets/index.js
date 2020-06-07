ad86bc114c0b0ba3ce8481d30935f2ff2850b57e
engrceey <ogbondachristian@gmail.com>
2020-06-07 21:14:20 +0000

    trying out solution for brackets

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,53 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  let countCurvedOpen = 0;
+  let countCurvedClosed = 0;
+
+  let countSquareOpen = 0;
+  let countSquareClosed = 0;
+
+  let countCurlyOpen = 0;
+  let countCurlyClosed = 0;
+
+  if (
+    str.includes("(}") ||
+    str.includes("{)") ||
+    str.includes("[}") ||
+    str.includes("{]") ||
+    str.includes("[)") ||
+    str.includes("(]")
+  ) {
+    return "invalid";
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(") {
+      countCurvedOpen++;
+    } else if (str[i] === ")") {
+      countCurvedClosed++;
+    } else if (str[i] === "{") {
+      countCurlyOpen++;
+    } else if (str[i] === "}") {
+      countCurlyClosed++;
+    } else if (str[i] === "[") {
+      countSquareOpen++;
+    } else if (str[i] === "]") {
+      countSquareClosed++;
+    }
+  }
+
+  if (
+    countCurlyOpen !== countCurlyClosed ||
+    countCurvedOpen !== countCurvedClosed ||
+    countSquareOpen !== countSquareClosed
+  ) {
+    return "invalid";
+  }
+
+  return "valid";
+}
+
 
 module.exports = isValid;
